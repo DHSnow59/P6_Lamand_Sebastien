adf152d8bc3598e143e256a10a4f5d994ed1608c
Sébastien <inoservice59@gmail.com>
2021-12-02 15:54:49 +0100

    Mise en place du Token UserID et initialisation des likes/dislikes a 0

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -3,7 +3,11 @@ const Sauce = require('../models/Sauce');
 exports.createSauce = (req, res, next) => {
     delete req.body._id;
     const sauce = new Sauce({
-        ...req.body
+        ...req.body,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
     });
     sauce.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
-//Enregistrement des Things dans la base de données
+//Enregistrement des Sauces dans la base de données
 router.post('/', stuffCtrl.createSauce);
 
-//Mettez à jour un Thing existant
+//Mettez à jour une sauce existant
 router.put('/:id', stuffCtrl.modifySauce);
-// Suppression d'un Thing
+// Suppression d'une sauce
 router.delete('/:id', stuffCtrl.deleteSauce);
-
-//Récupération d'un Thing spécifique
+//Récupération d'une sauce spécifique
 router.get('/:id', stuffCtrl.getOneSauce);
-
-
-//Récupération de la liste de Things en vente
+//Récupération de la liste des sauces en vente
 router.get('/', stuffCtrl.getAllSauce);
 
 module.exports = router;
